a851ee293f1100cdde7ee30d7602e9a7a7cbc3dc
Pragun <chhayakarande18@gmail.com>
2023-12-19 19:54:54 +0530

    added Models

---
+++ database/User.model.ts
@@ -0,0 +1,35 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IUser extends Document {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  bio?: string;
+  picture: string;
+  location?: string;
+  portfoliowebsite: string;
+  reputation: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
+}
+
+export const UserSchema = new Schema({
+  clerkId: { type: String, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  bio: { type: String },
+  picture: { type: String, required: true },
+  location: { type: String },
+  portfoliowebsite: { type: String, required: true },
+  reputation: { type: Number, default: 0 },
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+  joinedAt: { type: Date, default: Date.now },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
