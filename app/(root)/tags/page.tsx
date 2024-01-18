a706533846cca6c00b398cf0431b98f9ada3f1a6
Pragun <chhayakarande18@gmail.com>
2024-01-18 14:58:21 +0530

    add. Tags Page

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,67 @@
+import Filters from "@/components/shared/filters/Filters";
+import NoResults from "@/components/shared/noresults/NoResults";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { TagFilters } from "@/constants/filter";
+import { getTags } from "@/lib/ServerActions/Tags.action";
+import Link from "next/link";
+import React from "react";
+
+const Tag = async () => {
+  const allTags = await getTags({});
+
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 text-center sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Users</h1>
+      </div>
+
+      <div className="mt-4 gap-5  max-sm:flex-col sm:items-center md:mt-11">
+        <LocalSearchbar
+          route="/tags"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for Interesting Tags."
+          otherClasses="flex-1"
+        />
+
+        <Filters filter={TagFilters} />
+      </div>
+
+      <section className="mt-12 flex flex-wrap gap-4">
+        {allTags.length > 0 ? (
+          allTags.map((tag) => (
+            // <TagsCard key={tag.name} tag={tag}></TagsCard>
+            <Link
+              href={`/tags/${tag._id}`}
+              className="shadow-light100_darknone"
+              key={tag.name}
+            >
+              <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
+                <div className="h3-bold background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+                  <p className="paragraph-semibold text-dark300_light900">
+                    {tag.name}
+                  </p>
+                </div>
+                <p className="small-medium text-dark400_light500 mt-3.5">
+                  <span className="body-semibold primary-text-gradient mr-2.5">
+                    {tag.question.length}+
+                  </span>{" "}
+                  Questions
+                </p>
+              </article>
+            </Link>
+          ))
+        ) : (
+          <NoResults
+            title="Tags"
+            description="No tags here"
+            buttontext="Ask Questions"
+            path="/ask-question"
+          />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default Tag;
--- lib/ServerActions/Tags.action.ts
+++ lib/ServerActions/Tags.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/User.model";
 import { ConnectToDB } from "../Database/Mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/Tag.model";
 
 export async function getTopInteractedTags(tags: GetTopInteractedTagsParams) {
   try {
@@ -19,9 +20,25 @@ export async function getTopInteractedTags(tags: GetTopInteractedTagsParams) {
     return [
       { id: "1", name: "Tag1" },
       { id: "2", name: "Tag2" },
-
       { id: "3", name: "Tag3" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getTags(tags: GetAllTagsParams) {
+  try {
+    ConnectToDB();
+
+    const tag = await Tag.find({});
+
+    if (!tag) throw new Error("No User found");
+
+    console.log(tag);
+
+    return tag;
   } catch (error) {
     console.log(error);
     throw error;
